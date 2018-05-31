868091ebc1624aec238455d0d00c2085293e898f
dandinma <mdandini@esri.com>
2018-05-31 16:26:00 -0400

    Fixing issue preventing unit tests from running

--- geoportal/src/test/java/com/esri/geoportal/base/security/ArcGISAuthenticationProviderTest.java
+++ geoportal/src/test/java/com/esri/geoportal/base/security/ArcGISAuthenticationProviderTest.java
@@ -1,9 +1,14 @@
 package com.esri.geoportal.base.security;
 
+import java.lang.reflect.Field;
+
 import javax.json.Json;
 import javax.json.JsonObject;
 
+import com.esri.geoportal.context.GeoportalContext;
+
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Rule;
 import org.mockserver.client.server.MockServerClient;
 import org.mockserver.junit.MockServerRule;
@@ -12,6 +17,17 @@ public abstract class ArcGISAuthenticationProviderTest {
     @Rule
     public MockServerRule mockServerRule = new MockServerRule(this, 5000);
     protected MockServerClient mockServer;
+
+    @AfterClass
+    public static void tearDownAfterClass() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        // We have to clear out the Geoportal Context singleton in order for subsequent tests to work.
+        Field field = GeoportalContext.class.getDeclaredField("SINGLETON");
+        field.setAccessible(true);
+
+        field.set(null, null);
+        
+        field.setAccessible(false);
+    }
     
     @After
     public void tearDown() {
